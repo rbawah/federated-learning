83396cf22675a1289df776582eabe89726e2a94a
Rashid Bawah <arbawah1@gmail.com>
2023-04-11 21:51:01 -0600

    complete: iid implementation

--- main.py
+++ main.py
@@ -2,15 +2,17 @@
 def main():
 
     import torch
+    import torchvision
     import torch.nn as nn
     import torch.nn.functional as F
     import torch.optim as optim
     from torch.utils.data import random_split, DataLoader
     from torchvision import datasets, transforms
     import numpy as np
-    from tqdm import tqdm
+    # from tqdm import tqdm
 
     from models.global_model import GlobalModel
+    from utils.model_utils import client_update, global_aggregate, evaluate
 
     device = torch.device('cuda:0' if torch.cuda.is_available else 'cpu')
 
@@ -19,7 +21,7 @@ def main():
     print("Torchvision Version: ",torchvision.__version__)
 
     num_clients = 100
-    clients_selected = 10
+    num_selected = 10
     num_rounds = 5
     epochs = 5
     batch_size = 32
@@ -35,7 +37,7 @@ def main():
     test_loader = [DataLoader(test_set, batch_size=batch_size, shuffle=True)]
 
     server = GlobalModel().to(device)
-    clients = [GlobalModel().to(device) for i in range(clients_selected)]
+    clients = [GlobalModel().to(device) for i in range(num_selected)]
 
     for model in clients:
         model.load_state_dict(server.state_dict())
@@ -44,8 +46,18 @@ def main():
     client_optimizers = [optim.SGD(client.parameters(), lr=0.1) for client in clients]
 
     for round in range(num_rounds):
-        
+        client_idx = np.random.permutation(num_clients)[:num_selected]
+
+        loss = 0
+        for ii in range(num_selected):
+            loss += client_update(clients[ii], client_optimizers[ii], train_loader[client_idx[ii]], epochs=epochs, device=device)
+
+        global_aggregate(server, clients)
+        test_loss, test_accuracy = evaluate(server, test_loader)
+
+        print(f'Round {round}')
+        print(f'Avg Train Loss {(loss/num_selected):.3f} | Test Loss {test_loss:.3f} | Test Accuracy {test_accuracy:.3f}')
 
 
-
-
+if __name__ == '__main__':
+    main()
