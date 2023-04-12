9ae802e0906a7a84fe43b8b91e38ec79bb4e08fa
Rashid Bawah <arbawah1@gmail.com>
2023-04-12 15:25:39 -0600

    add: running model for non-i.i.d scenario

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
     import torch.nn as nn
     import torch.nn.functional as F
     import torch.optim as optim
-    from torch.utils.data import random_split, DataLoader
+    from torch.utils.data import random_split, DataLoader, Subset
     from torchvision import datasets, transforms
     import numpy as np
     # from tqdm import tqdm
@@ -29,6 +29,7 @@ def main():
     transform = transforms.Compose([transforms.ToTensor(),
                                     transforms.Normalize((0.1307,), (0.3081,))])
 
+    # Load and create datasets for i.i.d (homogenous) scenario
     train_set = datasets.MNIST('./data', train=True, download=True, transform=transform)
     test_set = datasets.MNIST('./data', train=False, download=True, transform=transform)
 
@@ -51,6 +52,29 @@ def main():
         loss = 0
         for ii in range(num_selected):
             loss += client_update(clients[ii], client_optimizers[ii], train_loader[client_idx[ii]], epochs=epochs, device=device)
+
+        global_aggregate(server, clients)
+        test_loss, test_accuracy = evaluate(server, test_loader, device=device)
+
+        print(f'Round {round}')
+        print(f'Avg Train Loss {(loss/num_selected):.3f} | Test Loss {test_loss:.3f} | Test Accuracy {test_accuracy:.3f}')
+
+    # Load and create datasets for non-i.i.d scenario
+    # Use same test_loader
+    labels_niid = torch.stack([train_set.targets == i for i in range(10)])
+    labels_split_niid = list()
+    for i in range(num_selected):
+        labels_split_niid += torch.split(torch.where(labels_niid[(2*i):(2*(i+1))].sum(0))[0], int(60000/num_clients))
+    
+    train_split = [Subset(train_set, ii) for ii in labels_split_niid]
+    train_loader = [DataLoader(data, batch_size=batch_size, shuffle=True) for data in train_split]
+
+    for round in range(num_rounds):
+        client_idx = np.random.permutation(num_clients)[:num_selected]
+
+        loss = 0
+        for ii in range(num_selected):
+            loss += client_update(clients[ii], client_optimizers[ii], train_loader, epochs=epochs, device=device)
 
         global_aggregate(server, clients)
         test_loss, test_accuracy = evaluate(server, test_loader, device=device)
